299b8c1e298ab4276321495f2dde72895ea34b15
hieupv <hieupv@codersvn.com>
2021-03-26 22:56:57 +0700

    lint

--- src/components/auth/services/role.service.ts
+++ src/components/auth/services/role.service.ts
@@ -2,8 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { BaseService } from '../../../shared/services/base.service';
 import { Repository, Connection } from 'typeorm';
 import { Role } from '../entities/role.entity';
-import { Permission } from '../entities/permission.entity';
-import { isEmpty } from 'lodash';
 
 @Injectable()
 export class RoleService extends BaseService {
